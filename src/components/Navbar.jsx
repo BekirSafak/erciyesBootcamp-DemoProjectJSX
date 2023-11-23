758af452825ffbc8c49cdc09aaadf87944663f08
Bekir SAFAK <bekir_safak_38@hotmail.com>
2023-11-23 16:29:50 +0300

    react-icons eklendi ve test edildi.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AiOutlineMenu } from "react-icons/ai";
+
+function Navbar() {
+    return (
+        <div>
+            <AiOutlineMenu></AiOutlineMenu>
+        </div>
+    )
+}
+
+export default Navbar
