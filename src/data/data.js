2a7e4096a1f611b580c7fea1c49a38ea4274251d
Bekir SAFAK <bekir_safak_38@hotmail.com>
2023-11-24 21:12:02 +0300

    Etkinlikler sayfasına Data.js içerisindeki bilgiler basıldı.

--- src/data/data.js
+++ src/data/data.js
@@ -3,84 +3,84 @@ export const data = [
         id: 1,
         name: 'MilyonFest Kayseri',
         category: 'concert',
-        image: './assets/camper.jpg',
+        image: require('../assets/concert.jpg'),
         price: 100
     },
     {
         id: 2,
         name: 'Çukurova Rock Festivali',
         category: 'concert',
-        image: './assets/camper.jpg',
+        image: require('../assets/concert1.jpg'),
         price: 150
     },
     {
         id: 3,
         name: "WRC Rally Turkey Marmaris",
         category: 'spor',
-        image: './assets/camper.jpg',
+        image: require('../assets/race1.png'),
         price: 800
     },
     {
         id: 4,
-        name: 'Formula 1 Turkish Grand Prix',
+        name: 'Formula 1 Turkish GP İstanbul',
         category: 'spor',
-        image: './assets/camper.jpg',
+        image: require('../assets/race.jpg'),
         price: 850
     },
     {
         id: 5,
         name: 'Kapadokya Çadır Kampı',
         category: 'camper',
-        image: './assets/camper.jpg',
+        image: require('../assets/camper2.jpg'),
         price: 300
     },
     {
         id: 6,
         name: 'PERA Konseri',
         category: 'concert',
-        image: './assets/camper.jpg',
+        image: require('../assets/singer1.jpg'),
         price: 350
     },
     {
         id: 7,
         name: 'Güldür Güldür Show',
         category: 'theatre',
-        image: './assets/camper.jpg',
+        image: require('../assets/theatre1.jpg'),
         price: 400
     },
     {
         id: 8,
         name: 'Çok Güzel Hareketler 2',
         category: 'theatre',
-        image: './assets/camper.jpg',
+        image: require('../assets/theatre.jpg'),
         price: 450
     },
     {
         id: 9,
         name: "90'lar Dans Gecesi",
         category: 'dance',
-        image: './assets/camper.jpg',
+        image: require('../assets/dance.jpg'),
         price: 500
     },
     {
         id: 10,
         name: 'Sahne Senin Karaoke Etkinliği',
         category: 'singer',
-        image: './assets/camper.jpg',
+        image: require('../assets/singer.jpg'),
         price: 550
     },
     {
         id: 11,
         name: 'Gesi Güzelköy Doğa Yürüyüşü',
         category: 'camper',
-        image: './assets/camper.jpg',
+        image: require('../assets/camper1.jpg'),
         price: 250
     },
     {
         id: 12,
         name: 'The Fast And The Furious ',
         category: 'cinema',
-        image: './assets/camper.jpg',
+        image: require('../assets/cinema.jpg'),
         price: 200
     },
 ]
