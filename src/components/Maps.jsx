364fcd95d6a0f9f02970fac08be4dae4647c28fb
Bekir SAFAK <bekir_safak_38@hotmail.com>
2023-11-28 22:41:41 +0300

    ActivitiesDetails sayfasındaki google maps lokasyon işaretleri dinamik olarak konuma özel getirildi.

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { useParams } from 'react-router-dom';
+import { data } from "../data/data.js";
+import { colors } from '@mui/material';
 
 function Maps(maps) {
     const { isLoaded } = maps
@@ -8,18 +11,22 @@ function Maps(maps) {
         height: '450px'
     };
 
+    const { id } = useParams();
+    let locationsID = data.find(q => q.id == id)
+    console.log(locationsID)
+    console.log(id)
 
-    const center = {
-        lat: 38.746,
-        lng: 35.530
-    };
+    // const center = {
+    //     lat: 38.746,
+    //     lng: 35.530
+    // };
     return isLoaded && (
         <GoogleMap
             mapContainerStyle={containerStyle}
-            center={center}
+            center={locationsID.location}
             zoom={10}
         >
-            <Marker position={center}></Marker>
+            <Marker position={locationsID.location}></Marker>
         </GoogleMap>
     )
 }
--- src/data/data.js
+++ src/data/data.js
@@ -4,83 +4,131 @@ export const data = [
         name: 'MilyonFest Kayseri',
         category: 'concert',
         image: require('../assets/concert.jpg'),
-        price: 100
+        price: 100,
+        location: {
+            lat: 38.7090,
+            lng: 35.5328
+        }
     },
     {
         "id": 2,
         name: 'Çukurova Rock Festivali',
         category: 'concert',
         image: require('../assets/concert1.jpg'),
-        price: 150
+        price: 150,
+        location: {
+            lat: 37.0138,
+            lng: 35.3294
+        }
     },
     {
         id: 3,
         name: "WRC Rally Turkey Marmaris",
         category: 'spor',
         image: require('../assets/race1.png'),
-        price: 800
+        price: 800,
+        location: {
+            lat: 36.8550,
+            lng: 28.2704
+        }
     },
     {
         id: 4,
         name: 'Formula 1 Turkish GP İstanbul',
         category: 'spor',
         image: require('../assets/race.jpg'),
-        price: 850
+        price: 850,
+        location: {
+            lat: 40.9526,
+            lng: 29.4063
+        }
     },
     {
         id: 5,
         name: 'Kapadokya Çadır Kampı',
         category: 'camper',
         image: require('../assets/camper2.jpg'),
-        price: 300
+        price: 300,
+        location: {
+            lat: 38.6523,
+            lng: 34.8236
+        }
     },
     {
         id: 6,
         name: 'PERA Konseri',
         category: 'music',
         image: require('../assets/singer1.jpg'),
-        price: 350
+        price: 350,
+        location: {
+            lat: 38.7499,
+            lng: 35.4910
+        }
     },
     {
         id: 7,
         name: 'Güldür Güldür Show',
         category: 'theatre',
         image: require('../assets/theatre1.jpg'),
-        price: 400
+        price: 400,
+        location: {
+            lat: 38.7499,
+            lng: 35.4910
+        }
     },
     {
         id: 8,
         name: 'Çok Güzel Hareketler 2',
         category: 'theatre',
         image: require('../assets/theatre.jpg'),
-        price: 450
+        price: 450,
+        location: {
+            lat: 38.7263,
+            lng: 35.4849
+        }
     },
     {
         id: 9,
         name: "90'lar Dans Gecesi",
         category: 'dance',
         image: require('../assets/dance.jpg'),
-        price: 500
+        price: 500,
+        location: {
+            lat: 38.5376,
+            lng: 35.5270
+        }
     },
     {
         id: 10,
         name: 'Sahne Senin Karaoke Etkinliği',
         category: 'singer',
         image: require('../assets/singer.jpg'),
-        price: 550
+        price: 550,
+        location: {
+            lat: 38.7102,
+            lng: 35.5541
+        }
     },
     {
         id: 11,
         name: 'Gesi Güzelköy Doğa Yürüyüşü',
         category: 'camper',
         image: require('../assets/camper1.jpg'),
-        price: 250
+        price: 250,
+        location: {
+            lat: 38.7689,
+            lng: 35.6653
+        }
     },
     {
         id: 12,
         name: 'The Fast And The Furious ',
         category: 'cinema',
         image: require('../assets/cinema.jpg'),
-        price: 200
+        price: 200,
+        location: {
+            lat: 38.7327,
+            lng: 35.5182
+        }
     },
 ]
