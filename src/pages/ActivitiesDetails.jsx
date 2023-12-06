3e1c4e638f31636738aa94caca4586351dfc4485
Bekir SAFAK <bekir_safak_38@hotmail.com>
2023-12-07 02:23:05 +0300

    Responsive düzenlemeler.

--- src/pages/ActivitiesDetails.jsx
+++ src/pages/ActivitiesDetails.jsx
@@ -23,7 +23,7 @@ function ActivitiesDetails() {
                 {/* Forms Start */}
                 <div className='shadow-lg shadow-purple-400 rounded-lg p-4'>
                     <div className="py-12 grid justify-items-center">
-                        <h2 className="text-purple-800 text-3xl font-bold">{activityID.name}</h2>
+                        <h2 className="text-purple-800 text-2xl md:text-3xl font-bold">{activityID.name}</h2>
                         <div className="mt-8 max-w-md grid grid-cols-1">
                             <div className="grid grid-cols-1 gap-6">
                                 <label className="block">
@@ -92,8 +92,8 @@ function ActivitiesDetails() {
                                     </div>
                                 </div>
                                 <div className='flex justify-between px-2 py-4 border-t-2'>
-                                    <Link className='bg-purple-400 hover:bg-purple-800 duration-300 text-white px-5 py-1 rounded-full border-none'>Rezervasyon Yap</Link>
-                                    <button className='bg-purple-800 hover:bg-purple-400 duration-300 text-white px-5 py-1 rounded-full border-none'>Satın Al</button>
+                                    <Link className='my-1 text-center text-xs md:text-base font-bold md:font-normal bg-purple-400 hover:bg-purple-800 duration-300 text-white px-3 py-1 rounded-full border-none'>Rezervasyon Yap</Link>
+                                    <button className='my-1 text-center text-xs md:text-base font-bold md:font-normal bg-purple-800 hover:bg-purple-400 duration-300 text-white px-3 py-1 rounded-full border-none'>Satın Al</button>
                                 </div>
                             </div>
 
